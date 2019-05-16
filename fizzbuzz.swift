bfc1203a59d3672540964a56596ce3b388829626
Jay Phillips <jayphillips@icloud.com>
2019-05-16 16:02:40 -0400

    Create fizzbuzz.swift

---
+++ fizzbuzz.swift
@@ -0,0 +1,13 @@
+for i in 1...100 {
+    if i % 3 == 0 {
+        if i % 5 == 0 {
+            print("FizzBuzz")
+        } else {
+            print("Fizz")
+        }
+    } else if i % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
